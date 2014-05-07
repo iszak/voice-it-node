58285a6c7f02d1f0edb6793ba38bb044dfbe11ec
Iszak Bryan <iszak@karmarama.com>
2014-05-07 22:12:20 +0000

    Started unit testing with mocha and chai

---
+++ test/voice-it.js
@@ -0,0 +1,39 @@
+var assert = require('chai').assert;
+
+suite('VoiceIt', function() {
+  var VoiceIt;
+
+  setup(function(){
+    VoiceIt = require('../lib/voice-it');
+  });
+
+
+  suite('#constructor', function(){
+    test('invalid options', function(){
+      assert.throws(
+        function(){
+          new VoiceIt();
+        },
+        Error,
+        'Options must be an object'
+      );
+    });
+
+    test('invalid developer id', function(){
+      assert.throws(
+        function(){
+          new VoiceIt({});
+        },
+        Error,
+        'Developer ID is required'
+      );
+    });
+
+
+    test('valid developer id', function(){
+      var instance = new VoiceIt({
+        developerId: 'sample-id'
+      });
+    });
+  });
+});
